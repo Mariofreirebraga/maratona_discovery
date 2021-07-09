b36185a048e2ab55c7001e18d7f0f1d3ecb0c70a
mariofreirebraga <mariomust25@gmail.com>
2021-07-09 19:16:24 -0300

    aprendendo scripts.js

---
+++ scripts.js
@@ -0,0 +1,93 @@
+const Modal = {
+    open(){
+        // Abrir modal
+        // Adicionar a class active ao modal
+        document
+            .querySelector('.modal-overlay')
+            .classList.add('active')
+    },
+    close(){
+        //fechar o modal
+        //remover a class active do modal
+        document
+            .querySelector('.modal-overlay')
+            .classList
+            .remove('active')
+    }
+}
+
+
+const transactions = [{
+    id:1,
+    description: 'luz',
+    amount: -50000,
+    date: '23/01/2021',
+    },
+    {
+        id: 2,
+        description: 'Website',
+        amount: 500000,
+        date: '23/01/2021'
+    },
+    {
+        id: 3,
+        description: 'Internet',
+        amount: -20000,
+        date: '23/01/2021'
+    },
+    {
+        id: 4,
+        description: 'App',
+        amount: 200000,
+        date: '23/01/2021'
+    }]
+
+
+const Transaction = {
+    incomes () {
+
+    },
+    expenses () {
+
+    },
+    total () {
+
+    }
+}
+
+const DOM = {
+    transactionsContainer:document.querySelector('#data-table tbody'),
+    addTransaction(transaction, index) {
+
+        const tr = document.createEvent('tr')
+        tr.innerHTML = DOM.innerHTMLTransaction(transaction)
+
+        DOM.transactionsContainer.appendChild(tr)
+    },
+    innerHTMLTransaction(transaction) {
+        const CSSclass = transaction.amount > 0 ? "income" : "expense"
+
+        const html = `
+
+        
+        <tr>
+        <td class="description">${transaction.description}</td>
+        <td class="${CSSclass}">${transaction.amount}</td>
+        <td class="date">${transaction.date}</td>
+        <td>
+        <img src="./assets/assets/minus.svg" alt="Remover transação">
+</td>
+</tr>
+        `
+
+        return html
+    }
+}
+
+DOM.addTransaction(transactions[0])
+DOM.addTransaction(transactions[1])
+DOM.addTransaction(transactions[2])
+
+transactions.forEach(function (transaction){
+    DOM.addTransaction(transaction)
+})
